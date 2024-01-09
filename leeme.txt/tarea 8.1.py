18750779ae4509b2ad0c590f9c78fafa184ecfaf
DaniEstepa <150685128+DanielEstepa@users.noreply.github.com>
2024-01-09 12:21:36 +0100

    Add files via upload

---
+++ leeme.txt/tarea 8.1.py
@@ -0,0 +1,9 @@
+O=input("Ingrese una oracion")
+x=
+V=0
+F=O.lower()
+while x<len(F)
+    if F[X]=="a" or F[x]=="e" or F[x]=="o" or F[x]=="u":
+        V=V+14
+    x=x+1
+print("hay",V, "vocales")
---
+++ leeme.txt/tarea 8.py
@@ -0,0 +1,9 @@
+O=input("Ingresa una oracion:")
+x=0
+C=0
+while x<len(O):
+
+    if O[x]==" ":
+        C=C+1
+    x=x+1
+print("En esta oracion hay:",C+1,"palabras")
